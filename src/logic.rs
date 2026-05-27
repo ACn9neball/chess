87b4008cadc3d76141d13b3a574301b56e163793
AC Nine <bhekainnocentn@gmail.com>
2026-05-27 11:45:01 +0200

    Can't move piece over piece

--- src/logic.rs
+++ src/logic.rs
@@ -9,13 +9,18 @@ pub fn w_pawn(
     if nrow > 0 {
         if board[nrow][ncol] == ".".to_string() {
             if row == 6 && (nrow == 5 || nrow == 4) && col == ncol {
-                board[nrow][ncol] = value;
-                board[row][col] = ".".to_string();
+                if nrow == 4 && board[5][ncol] == ".".to_string() {
+                    board[nrow][ncol] = value;
+                    board[row][col] = ".".to_string();
+                } else if nrow == 5 {
+                    board[nrow][ncol] = value;
+                    board[row][col] = ".".to_string();
+                }
             } else if nrow == (row - 1) && col == ncol {
                 board[nrow][ncol] = value;
                 board[row][col] = ".".to_string();
+            } else if nrow == 0 {
             }
-        } else if nrow == 0 {
         }
     }
 
@@ -33,13 +38,18 @@ pub fn b_pawn(
     if nrow < 7 {
         if board[nrow][ncol] == ".".to_string() {
             if row == 1 && (nrow == 2 || nrow == 3) && col == ncol {
-                board[nrow][ncol] = value;
-                board[row][col] = ".".to_string();
+                if nrow == 3 && board[2][ncol] == ".".to_string() {
+                    board[nrow][ncol] = value;
+                    board[row][col] = ".".to_string();
+                } else if nrow == 2 {
+                    board[nrow][ncol] = value;
+                    board[row][col] = ".".to_string();
+                }
             } else if nrow == (row + 1) && col == ncol {
                 board[nrow][ncol] = value;
                 board[row][col] = ".".to_string();
+            } else if nrow == 7 {
             }
-        } else if nrow == 7 {
         }
     }
 
@@ -54,13 +64,39 @@ pub fn rook(
     ncol: usize,
 ) -> [[String; 8]; 8] {
     let value = board[row][col].clone();
+    let count_row = nrow.abs_diff(row);
+    let count_col = ncol.abs_diff(col);
+    let mut found = false;
+
     if board[nrow][ncol] == ".".to_string() {
         if col == ncol {
-            board[nrow][ncol] = value;
-            board[row][col] = ".".to_string();
+            let row_dir = if nrow > row { 1 as isize } else { -1 as isize };
+            let mut current_row = row as isize;
+            for _ in 1..count_row {
+                current_row += row_dir;
+
+                if board[current_row as usize][ncol] != ".".to_string() {
+                    found = true;
+                }
+            }
+            if !found {
+                board[nrow][ncol] = value;
+                board[row][col] = ".".to_string();
+            }
         } else if row == nrow {
-            board[nrow][ncol] = value;
-            board[row][col] = ".".to_string();
+            let col_dir = if ncol > col { 1 as isize } else { -1 as isize };
+            let mut current_col = col as isize;
+            for _ in 1..count_col {
+                current_col += col_dir;
+
+                if board[nrow][current_col as usize] != ".".to_string() {
+                    found = true;
+                }
+            }
+            if !found {
+                board[nrow][ncol] = value;
+                board[row][col] = ".".to_string();
+            }
         }
     }
 
@@ -77,10 +113,27 @@ pub fn bishop(
     let value = board[row][col].clone();
     let count_row = nrow.abs_diff(row);
     let count_col = ncol.abs_diff(col);
+    let mut found = false;
+
     if board[nrow][ncol] == ".".to_string() {
         if count_row == count_col {
-            board[nrow][ncol] = value;
-            board[row][col] = ".".to_string();
+            let row_dir = if nrow > row { 1 as isize } else { -1 as isize };
+            let col_dir = if ncol > col { 1 as isize } else { -1 as isize };
+            let mut current_row = row as isize;
+            let mut current_col = col as isize;
+            for _ in 1..count_row {
+                current_row += row_dir;
+                current_col += col_dir;
+
+                if board[current_row as usize][current_col as usize] != ".".to_string() {
+                    found = true;
+                }
+            }
+
+            if !found {
+                board[nrow][ncol] = value;
+                board[row][col] = ".".to_string();
+            }
         }
     }
 
@@ -117,16 +170,56 @@ pub fn queen(
     let value = board[row][col].clone();
     let count_row = nrow.abs_diff(row);
     let count_col = ncol.abs_diff(col);
+    let mut found = false;
+
     if board[nrow][ncol] == ".".to_string() {
         if count_row == count_col {
-            board[nrow][ncol] = value;
-            board[row][col] = ".".to_string();
+            let row_dir = if nrow > row { 1 as isize } else { -1 as isize };
+            let col_dir = if ncol > col { 1 as isize } else { -1 as isize };
+            let mut current_row = row as isize;
+            let mut current_col = col as isize;
+            for _ in 1..count_row {
+                current_row += row_dir;
+                current_col += col_dir;
+
+                if board[current_row as usize][current_col as usize] != ".".to_string() {
+                    found = true;
+                }
+            }
+
+            if !found {
+                board[nrow][ncol] = value;
+                board[row][col] = ".".to_string();
+            }
         } else if nrow == row {
-            board[nrow][ncol] = value;
-            board[row][col] = ".".to_string();
+            let col_dir = if ncol > col { 1 as isize } else { -1 as isize };
+            let mut current_col = col as isize;
+            for _ in 1..count_col {
+                current_col += col_dir;
+
+                if board[nrow][current_col as usize] != ".".to_string() {
+                    found = true;
+                }
+            }
+            if !found {
+                board[nrow][ncol] = value;
+                board[row][col] = ".".to_string();
+            }
         } else if ncol == col {
-            board[nrow][ncol] = value;
-            board[row][col] = ".".to_string();
+            let row_dir = if nrow > row { 1 as isize } else { -1 as isize };
+            let mut current_row = row as isize;
+            for _ in 1..count_row {
+                current_row += row_dir;
+
+                if board[current_row as usize][ncol] != ".".to_string() {
+                    found = true;
+                }
+            }
+
+            if !found {
+                board[nrow][ncol] = value;
+                board[row][col] = ".".to_string();
+            }
         }
     }
 
